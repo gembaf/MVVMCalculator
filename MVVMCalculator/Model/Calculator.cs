ce5c5c575eacfcaa2864400b3d8d6b9331fadf57
gembaf <s11t230@stmail.eng.kagawa-u.ac.jp>
2015-04-08 21:14:40 +0900

    計算の内部処理を実装

---
+++ MVVMCalculator/Model/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMCalculator.Model
+{
+    public class Calculator
+    {
+        #region enum Type
+        public enum Type
+        {
+            None,
+            Add,
+            Sub,
+            Mul,
+            Div
+        }
+        #endregion
+
+        public double Execute(double left, double right, Type type)
+        {
+            double result = 0;
+
+            switch (type)
+            {
+                case Type.Add:
+                    result = left + right;
+                    break;
+                case Type.Sub:
+                    result = left - right;
+                    break;
+                case Type.Mul:
+                    result = left * right;
+                    break;
+                case Type.Div:
+                    result = (right == 0) ? 0 : left / right;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MVVMCalculatorTest/Model/CalculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using MVVMCalculator.Model;
+
+namespace MVVMCalculatorTest.Model
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [TestCase(4, 2, Result = 2)]
+        [TestCase(3, 2, Result = 1.5)]
+        [TestCase(4, 0, Result = 0)]
+        public double ExecuteTest割り算(double left, double right)
+        {
+            Calculator calc = new Calculator();
+            return calc.Execute(left, right, Calculator.Type.Div);
+        }
+    }
+}
