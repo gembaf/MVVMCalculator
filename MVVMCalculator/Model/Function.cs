b740fd6023c6074c885de22b36979a38468b4dce
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-04-09 21:14:14 +0900

    方程式のモデルを定義
    Calculatorクラスに計算を委譲するメソッドを追加

---
+++ MVVMCalculator/Model/Function.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMCalculator.Model
+{
+    public class Function
+    {
+        public double Left { get; set; }
+
+        public double Right { get; set; }
+
+        public Calculator.Type CalculateType { get; set; }
+
+        public double Calculate()
+        {
+            Calculator calc = new Calculator();
+            return calc.Execute(Left, Right, CalculateType);
+        }
+    }
+}
---
+++ MVVMCalculatorTest/Model/FunctionTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using MVVMCalculator.Model;
+
+namespace MVVMCalculatorTest.Model
+{
+    [TestFixture]
+    public class FunctionTest
+    {
+        [TestCase(3, 5, Calculator.Type.Add, Result = 8)]
+        [TestCase(2, 4, Calculator.Type.Mul, Result = 8)]
+        public double CalculateTest(double a, double b, Calculator.Type type)
+        {
+            Function func = new Function {
+                Right = a,
+                Left = b,
+                CalculateType = type
+            };
+            return func.Calculate();
+        }
+    }
+}
